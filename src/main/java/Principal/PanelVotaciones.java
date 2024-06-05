2dcb023f9af5ad1710cefcb047b7378e1e702f33
IsaacDerivia <22bl7208718@unedl.mx>
2024-06-05 13:20:14 -0600

    crear proyecto

---
+++ src/main/java/Principal/PanelVotaciones.java
@@ -0,0 +1,8 @@
+package Principal;
+import javax.swing.*;
+public class PanelVotaciones extends JPanel {
+
+
+
+
+}
---
+++ src/main/java/Principal/Votaciones.java
@@ -0,0 +1,5 @@
+package Principal;
+import javax.swing.*;
+public class Votaciones extends JFrame {
+
+}
